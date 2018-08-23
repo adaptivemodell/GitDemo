a7b8d8d60f491728bd6a190948c7cd7bd84c700d
adaptivemodell <48nice@gmail.com>
2018-08-24 00:05:26 +0300

    Update View Controller With One Print

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,14 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Another print statement")
-        
+        print("Hello Git")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
